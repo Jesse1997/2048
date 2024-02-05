3f0a1839e8a3f5a2ac2a7666eab290c87af5dc40
Jesse Van Loock <jessevanloock@gmail.com>
2024-02-05 12:57:05 +0100

    add initial .net maui setup

---
+++ 2048/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _2048
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ 2048/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _2048
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
